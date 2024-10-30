51b9d1a749d5367231a5890bba8f3d862cefbbb5
Pawan-katuwal <powankc285@gmail.com>
2024-10-30 15:07:19 +0545

    Add files via upload

---
+++ constant.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#define constant 7
+int main(){
+    
+    int b;
+    printf("The constant value of a is:%d\n",constant);
+    b=constant+3;
+    printf("\nThe sum of a nd 3 is :%d",b);
+    return 0;
+}
